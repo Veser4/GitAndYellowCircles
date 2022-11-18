eddec4c6e92e476b1b92195963c5fa9e3ee22466
veser <Veser2004@gmail.com>
2022-11-18 17:16:45 +0300

    MakeCircle(RandomColor)

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QPlainTextEdit
+from PyQt5 import QtCore, QtGui, QtWidgets
 import random
 import sys
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QColor
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(280, 220, 211, 71))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Push me to make a Circle"))
 
 
-class MyWidget(QMainWindow):
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+class FirstForm(QMainWindow, Ui_MainWindow):
+
     def __init__(self):
-        super().__init__()
-        uic.loadUi('UI.ui', self)
+        super(FirstForm, self).__init__()
+        self.setupUi(self)
+        self.initUI()
+
+    def initUI(self):
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
-
 
     def paintEvent(self, event):
         if self.do_paint:
@@ -26,13 +63,13 @@ class MyWidget(QMainWindow):
         self.repaint()
 
     def draw_Circle(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randint(0, 254), random.randint(0, 254), random.randint(0, 254)))
         radius = random.randint(10, 60)
         qp.drawEllipse(random.randint(0, self.width()), random.randint(0, self.height()), radius, radius)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = FirstForm()
     ex.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
